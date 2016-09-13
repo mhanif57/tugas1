b20117d87bb66385f721a1d915b23b7db00fc189
mhanif57 <m.hanief97@gmail.com>
2016-09-13 14:06:07 +0700

    Add files via upload

---
+++ TUGAS_1.ino
@@ -0,0 +1,33 @@
+int i=0;
+void setup() {
+DDRC=00;
+PORTC=05;
+
+DDRB=15;
+}
+
+void loop() {
+  if (digitalRead(A0)==LOW)
+  {
+    for (int led=i;led<=15;led++)
+    PORTB=led;
+    delay(1000);
+  
+  }
+  else if (digitalRead(A1)==LOW)
+  {
+    for (int led=i;led<=15;led--)
+    PORTB=led;
+    delay(1000);
+  }
+
+  
+   else if (digitalRead(A2)==LOW)
+  {
+    PORTB=15;
+    delay(1000);
+    PORTB=00;
+  }
+  
+
+}
